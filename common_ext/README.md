79e9258a1827a0310f472053577926dda102c659
Oleksandr Pavlyk <oleksandr.pavlyk@intel.com>
2022-07-08 15:42:25 -0500

    Used license headers

---
+++ common_ext/README.md
@@ -0,0 +1,4 @@
+
+Common files implementing Python native extension reused via symbolic links
+by `kde_setuptools` and `kde_skbuild` examples of build data-parallel Python native
+extensions with oneAPI DPC++.
--- common_ext/py_kde.cpp
+++ common_ext/py_kde.cpp
@@ -1,3 +1,20 @@
+// Copyright 2022 Intel Corporation
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+// File implementing pybind11-driven native Python extension exposing
+// data-parallel function to compute Kernel Density Estimation using oneAPI
+
 #include <CL/sycl.hpp>
 #include <cstdint>
 #include <limits>
@@ -24,10 +41,13 @@ py_kde_eval_t(
     }
 
     auto dim = x_pybuf.shape[1];
-    constexpr auto dim_max = static_cast<decltype(dim)>(std::numeric_limits<std::uint16_t>::max());
+    constexpr auto dim_max = static_cast<decltype(dim)>(
+	std::numeric_limits<std::uint16_t>::max());
 
     if (dim != data_pybuf.shape[1] || dim > dim_max) {
-	throw py::value_error("Inputs have inconsistent functionality or too large a width");
+	throw py::value_error(
+	    "Inputs have inconsistent functionality or too large a width"
+	    );
     }
 
     auto n = x_pybuf.shape[0];
